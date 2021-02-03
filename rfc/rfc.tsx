68e75ed09d12d36ba282b6491dc8dc9cb6b39b33
nitzanmo <nitzanmousan@gmail.com>
2021-02-03 14:20:27 +0200

    Create rfc.tsx

---
+++ rfc/rfc.tsx
@@ -0,0 +1,21 @@
+import {makeStyles, useTheme} from "@material-ui/core/styles";
+import React, {FC} from "react";
+
+const useStyles = makeStyles((theme) => ({
+}))
+
+interface $class$Props {
+
+}
+
+const $class$: FC<$class$Props> = ({}) => {
+    const theme = useTheme();
+    const classes = useStyles(theme);
+    
+    return (
+            <>
+            </>
+    )
+}
+    
+export default $class$;
